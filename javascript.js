8325114ec8cbf56a43a7cf733a09b96897672bda
LetsBuildAFlyingDonkey <48519981+U2FUSB@users.noreply.github.com>
2025-06-27 14:00:07 +0200

    feat: Add rand-id for every new book

--- javascript.js
+++ javascript.js
@@ -1,1 +1,11 @@
-console.log("test")
+const myLibrary = [];
+
+function Book() {
+    // the constructor...
+    this.id = crypto.randomUUID()
+}
+
+function addBookToLibrary() {
+    // take params, create a book then store it in the array
+}
+const book1 = new Book();
